e43dde5337d0ea6a21a5b5c337b429497c9a76a5
Craig Wendel <wendelcraig@gmail.com>
2017-08-08 14:21:04 -0500

    Initial commit. Linked previous info from starter files and page compiles, need to map through user details.

---
+++ src/containers/AccountDetail.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {Link} from 'react-router-dom'
+import {selectAccount} from '../actions/index'
+
+class AccountDetail extends Component {
+  render () {
+    return (
+      <div className='account-detail'>
+        <h4>Account Detail</h4>
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps(state) {
+  const userIdx = state.users.findIndex(user => user._id === state.selectedUser);
+  const accountIdx = state.users[userIdx].accounts.findIndex(account => account.id === state.selectedAccount);
+  return {
+    account: state.users[userIdx].accounts[accountIdx],
+    user: state.users[userIdx]
+  };
+}
+
+export default connect(mapStateToProps)(AccountDetail)
